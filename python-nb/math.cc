af6bda56c78dd4a9bd32815e773af0f9a7616e08
ManifoldFR <wilson.jallet@polytechnique.org>
2025-02-17 20:08:03 +0100

    python-nb : Start work on math module
    Add more to Transform3s binding

---
+++ python-nb/math.cc
@@ -0,0 +1,79 @@
+/// Copyright 2025 INRIA
+#include "coal/math/transform.h"
+
+#include "fwd.h"
+#include <nanobind/eigen/dense.h>
+#include <nanobind/stl/bind_vector.h>
+#include <nanobind/operators.h>
+
+using namespace coal;
+using namespace nb::literals;
+
+void exposeMaths(nb::module_ &m) {
+  nb::module_::import_("nanoeigenpy");
+
+  nb::class_<Transform3s>(m, "Transform3s")
+      .def(nb::init<>())
+      .def(nb::init<const Matrix3s &, Vec3s>(), "R"_a, "t"_a)
+      .def(nb::init<const Quatf &, Vec3s>(), "q"_a, "t"_a)
+      .def(nb::init<const Matrix3s &>(), "R"_a)
+      .def(nb::init<const Quatf &>(), "q"_a)
+      .def(nb::init<const Vec3s &>(), "t"_a)
+      .def(nb::init<const Transform3s &>())
+      .def("getQuatRotation", &Transform3s::getQuatRotation,
+           nb::rv_policy::automatic_reference)
+      .def("setQuatRotation", &Transform3s::setQuatRotation)
+
+      .def("getTranslation", &Transform3s::getTranslation,
+           nb::rv_policy::automatic_reference)
+      .def_prop_rw("translation", &Transform3s::getTranslation,
+                   [](Transform3s &t, Vec3s v) { t.setTranslation(v); })
+
+      .def("getRotation", &Transform3s::getRotation)
+      .def_prop_rw("rotation", &Transform3s::getRotation,
+                   [](Transform3s &t, Matrix3s R) { t.setRotation(R); })
+
+      // inverse
+      .def("inverseInPlace", &Transform3s::inverseInPlace,
+           nb::rv_policy::automatic_reference)
+      .def("inverse", &Transform3s::inverse)
+      .def("inverseTimes", &Transform3s::inverseTimes)
+
+      // identity
+      .def("isIdentity", &Transform3s::isIdentity)
+      .def("setIdentity", &Transform3s::setIdentity)
+      .def_static("Identity", &Transform3s::Identity)
+
+      .def("setRandom", &Transform3s::setRandom)
+      .def_static("Random", &Transform3s::Random)
+
+      .def(nb::self * nb::self)
+      .def(nb::self *= nb::self)
+      .def(nb::self == nb::self)
+      .def(nb::self != nb::self);
+
+  nb::class_<Triangle>(m, "Triangle")
+      .def(nb::init<>())
+      .def(nb::init<Triangle::index_type, Triangle::index_type,
+                    Triangle::index_type>(),
+           "p1"_a, "p2"_a, "p3"_a)
+      .def("__getitem__",
+           [](Triangle &m, Py_ssize_t i) {
+             if (i >= 3 || i <= -3) {
+               throw nb::index_error("Index out of range");
+             }
+             return m[static_cast<Triangle::index_type>(i % 3)];
+           })
+      .def("__setitem__",
+           [](Triangle &m, Py_ssize_t i, Triangle::index_type v) {
+             if (i >= 3 || i <= -3) {
+               throw nb::index_error("Index out of range");
+             }
+             m[static_cast<Triangle::index_type>(i % 3)] = v;
+           })
+      .def("set", &Triangle::set)
+      .def_static("size", &Triangle::size)
+      .def(nb::self == nb::self);
+
+  nb::bind_vector<std::vector<Triangle>>(m, "StdVec_Triangle");
+}
