4be5298ec366a9ef71d0dd88d61f5f39216cd0a3
ManifoldFR <wilson.jallet@polytechnique.org>
2025-03-24 11:44:00 +0100

    Make building Nanobind bindings an "or" rather and an "either"

---
+++ python-nb/coal/__init__.py
@@ -0,0 +1,3 @@
+# ruff: noqa: F401, F403
+from .coal_pywrap_nb import *  # noqa
+from .coal_pywrap_nb import __version__
--- python-nb/coal/v2/__init__.py
+++
@@ -1,3 +0,0 @@
-# ruff: noqa: F401, F403
-from .coal_pywrap_v2 import *  # noqa
-from .coal_pywrap_v2 import __version__
