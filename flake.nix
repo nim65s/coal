e1ade63be41031ff6bd9a87111ece70a8b87fc37
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-05-13 16:38:12 +0200

    nix: nanoeigenpy

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # TODO: switch back to nixos-unstable after
+    # https://github.com/NixOS/nixpkgs/pull/395016
+    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/395016/head";
   };
 
   outputs =
@@ -20,7 +22,17 @@
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
             default = self'.packages.coal;
-            coal = pkgs.python3Packages.coal.overrideAttrs (_: {
+            coal = pkgs.python3Packages.coal.overrideAttrs (super: {
+              postPatch = ''
+                substituteInPlace python-nb/CMakeLists.txt --replace-fail \
+                  "$""{Python_SITELIB}" \
+                  "${pkgs.python3.sitePackages}"
+              '';
+              propagatedBuildInputs = super.propagatedBuildInputs ++ [
+                pkgs.python3Packages.nanobind
+                pkgs.python3Packages.nanoeigenpy
+              ];
+              pythonImportsCheck = [ "coal" ]; # hppfcl is broken with nanobind
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
@@ -30,6 +42,7 @@
                   ./include
                   ./package.xml
                   ./python
+                  ./python-nb
                   ./src
                   ./test
                 ];
