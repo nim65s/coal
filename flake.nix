329add676d6f0c72282e4b055884dea09e0e2a95
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-03 17:24:22 +0200

    nix: disable hpp-fcl warnings

--- flake.nix
+++ flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
-    # TODO: switch back to nixos-unstable after
-    # https://github.com/NixOS/nixpkgs/pull/357705
-    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/357705/head";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
@@ -22,7 +20,8 @@
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
             default = self'.packages.coal;
-            coal = pkgs.python3Packages.coal.overrideAttrs (_: {
+            coal = pkgs.python3Packages.coal.overrideAttrs (super: {
+              cmakeFlags = super.cmakeFlags ++ [ "-DCOAL_DISABLE_HPP_FCL_WARNINGS=ON" ];
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
