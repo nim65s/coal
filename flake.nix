0f92979e065b2bc980f34e484dcd5c0d8fb7f8c8
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-25 11:26:45 +0200

    Nix: full / split

--- flake.nix
+++ flake.nix
@@ -17,28 +17,43 @@
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
           };
-          devShells.default =
-            with pkgs;
-            mkShell {
-              inputsFrom = [ pkgs.python3Packages.coal ];
-              packages =
-                let
-                  py = p: [
-                    p.boost
-                    p.eigenpy
-                    p.numpy
-                    p.scipy
-                  ];
-                in
-                [
-                  (python312.withPackages py)
-                  (python313.withPackages py)
-                ];
-            };
           packages = {
             default = self'.packages.coal;
-            coal = pkgs.coal.overrideAttrs (super: {
-              cmakeFlags = super.cmakeFlags ++ [ "-DCOAL_DISABLE_HPP_FCL_WARNINGS=ON" ];
+            coal = pkgs.python3Packages.toPythonModule (
+              (pkgs.coal.override { pythonSupport = true; }).overrideAttrs (super: {
+                cmakeFlags = super.cmakeFlags ++ [ "-DCOAL_DISABLE_HPP_FCL_WARNINGS=ON" ];
+                src = pkgs.lib.fileset.toSource {
+                  root = ./.;
+                  fileset = pkgs.lib.fileset.unions [
+                    ./CMakeLists.txt
+                    ./doc
+                    ./hpp-fclConfig.cmake
+                    ./include
+                    ./package.xml
+                    ./python
+                    ./src
+                    ./test
+                  ];
+                };
+              })
+            );
+            coal-cpp = (self'.packages.coal.override { pythonSupport = false; }).overrideAttrs (super: {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./CMakeLists.txt
+                  ./doc
+                  ./hpp-fclConfig.cmake
+                  ./include
+                  ./package.xml
+                  # ./python
+                  ./src
+                  ./test
+                ];
+              };
+            });
+            coal-py = (self'.packages.coal.override { pythonSupport = true; }).overrideAttrs (super: {
+              cmakeFlags = super.cmakeFlags ++ [ "-DBUILD_ONLY_PYTHON_INTERFACE=ON" ];
               src = pkgs.lib.fileset.toSource {
                 root = ./.;
                 fileset = pkgs.lib.fileset.unions [
@@ -48,30 +63,14 @@
                   ./include
                   ./package.xml
                   ./python
-                  ./src
+                  # ./src
                   ./test
                 ];
               };
+              propagatedBuildInputs = super.propagatedBuildInputs ++ [
+                self'.packages.coal-cpp
+              ];
             });
-            py-coal =
-              (self'.packages.coal.override {
-                inherit (pkgs) python3Packages;
-                pythonSupport = true;
-              }).overrideAttrs
-                (super: {
-                  cmakeFlags = super.cmakeFlags ++ [
-                    "-DBUILD_ONLY_PYTHON_PYTHON_INTERFACE=ON"
-                  ];
-                  propagatedBuildInputs = super.propagatedBuildInputs ++ [
-                    self'.packages.coal
-                  ];
-                });
-            py312-coal = self'.packages.py-coal.override {
-              python3Packages = pkgs.python312Packages;
-            };
-            py313-coal = self'.packages.py-coal.override {
-              python3Packages = pkgs.python313Packages;
-            };
           };
         };
     };
